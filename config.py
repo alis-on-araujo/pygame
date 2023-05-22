c860524985e7b039fac74cfb71d09e00053b5b3f
thaissabaini <thaissc3@al.insper.edu.br>
2023-05-22 16:17:47 -0300

    configurações do jogo

---
+++ config.py
@@ -0,0 +1,24 @@
+#Largura e altura do fundo
+largura = 700
+altura = 850
+
+
+FPS = 30
+
+#Largura e altura do pássaro
+passaro_largura = 50
+passaro_altura = 38
+
+#Largura e altura da casa
+casa_largura = 100
+casa_altura = 240
+
+
+#Variáveis para cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
